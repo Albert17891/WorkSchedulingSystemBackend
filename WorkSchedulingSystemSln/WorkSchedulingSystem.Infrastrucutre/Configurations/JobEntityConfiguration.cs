d259ebef1f883002354d16e0cc47ebb26ddbe99c
Gevorkyan Albert <abogevorgian@gmail.com>
2025-12-01 16:49:42 +0400

    add job and schedule entity configuration

---
+++ WorkSchedulingSystemSln/WorkSchedulingSystem.Infrastrucutre/Configurations/JobEntityConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WorkSchedulingSystem.Domain.Entities;
+
+public class JobEntityConfiguration : IEntityTypeConfiguration<Job>
+{
+    public void Configure(EntityTypeBuilder<Job> builder)
+    {
+        builder.ToTable("Jobs");
+
+        builder.HasKey(j => j.Id);
+
+        builder.Property(j => j.Id)
+            .ValueGeneratedNever();
+
+        builder.Property(j => j.Title)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(j => j.Description)
+            .HasMaxLength(2000);
+
+        builder.Property(j => j.Duration)
+            .IsRequired();
+
+        builder.Property(j => j.ScheduledDate)
+            .IsRequired();
+
+        builder.Property(j => j.AssignedUserId)
+            .IsRequired();
+    }
+}
---
+++ WorkSchedulingSystemSln/WorkSchedulingSystem.Infrastrucutre/Configurations/ScheduleEntityConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WorkSchedulingSystem.Domain.Entities;
+
+public class ScheduleEntityConfiguration : IEntityTypeConfiguration<Schedule>
+{
+    public void Configure(EntityTypeBuilder<Schedule> builder)
+    {
+        builder.ToTable("Schedules");
+
+        builder.HasKey(s => s.Id);
+
+        builder.Property(s => s.Id)
+            .ValueGeneratedNever();      
+
+        builder.Property(s => s.UserId)
+            .IsRequired();
+
+        builder.Property(s => s.Date)
+            .IsRequired();
+
+        builder.Property(s => s.Status)
+            .IsRequired()
+            .HasConversion<int>();      
+
+       
+        builder.HasMany(s => s.Jobs)
+               .WithOne()
+               .HasForeignKey(j => j.AssignedUserId)   
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Navigation(s => s.Jobs)
+               .UsePropertyAccessMode(PropertyAccessMode.Field);
+    }
+}
