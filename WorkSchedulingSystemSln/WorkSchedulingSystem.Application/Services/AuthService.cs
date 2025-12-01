84306b7c543b97e9903b0933fbd4ba3e1a7a6cc3
Gevorkyan Albert <abogevorgian@gmail.com>
2025-12-01 18:10:20 +0400

    implement services auth and user

---
+++ WorkSchedulingSystemSln/WorkSchedulingSystem.Application/Services/AuthService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WorkSchedulingSystem.Application.Common.Settings;
+using WorkSchedulingSystem.Application.ServiceContracts;
+using WorkSchedulingSystem.Domain.Entities;
+
+namespace WorkSchedulingSystem.Application.Services;
+
+public class AuthService : IAuthService
+{
+    private readonly JwtSettings _jwtSettings;
+    public AuthService(IOptions<JwtSettings> jwtSettings)
+    {
+        _jwtSettings = jwtSettings.Value;
+    }
+
+    public async Task<string> GenerateJwtTokenAsync(User user, UserManager<User> userManager)
+    {
+        var roles = await userManager.GetRolesAsync(user);
+
+        var claims = new List<Claim>
+         {
+           new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+           new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+           new Claim(ClaimTypes.NameIdentifier, user.Id),
+           new Claim(ClaimTypes.Name, user.UserName)
+         };
+
+        foreach (var role in roles)
+        {
+            claims.Add(new Claim(ClaimTypes.Role, role));
+        }
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(30),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+}
---
+++ WorkSchedulingSystemSln/WorkSchedulingSystem.Application/Services/UserService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WorkSchedulingSystem.Application.Common;
+using WorkSchedulingSystem.Application.DTO.User;
+using WorkSchedulingSystem.Application.ServiceContracts;
+using WorkSchedulingSystem.Domain.Entities;
+
+namespace WorkSchedulingSystem.Application.Services;
+
+public class UserService : IUserService
+{
+    private readonly UserManager<User> _userManager;
+    private readonly IAuthService _authService;
+
+    public UserService(UserManager<User> userManager, IAuthService authService)
+    {
+        _userManager = userManager;
+        _authService = authService;
+    }
+
+    public async Task<ApiResult<User>> CreateUserAsync(UserRegisterDto userRegister)
+    {
+        var user = User.Create(
+             userRegister.FirstName,
+             userRegister.LastName,
+             userRegister.Gender,
+             userRegister.Email,
+             userRegister.BirthDate.ToUniversalTime());
+
+        var result = await _userManager.CreateAsync(user, userRegister.Password);
+
+        if (!result.Succeeded)
+        {
+            return ApiResult<User>.Failure("User creation failed");
+        }
+
+        return ApiResult<User>.Success(user);
+    }
+
+    public async Task<string> LoginAsync(LoginDto loginDto)
+    {
+        var user = await _userManager.FindByNameAsync(loginDto.UserName);
+
+        if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password))
+        {
+            throw new UnauthorizedAccessException("Invalid email or password.");
+        }
+
+        var token = await _authService.GenerateJwtTokenAsync(user, _userManager);
+
+        return token;
+    }
+}
