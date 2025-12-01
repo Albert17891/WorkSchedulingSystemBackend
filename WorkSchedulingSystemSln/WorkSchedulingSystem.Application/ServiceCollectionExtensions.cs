ff1155e4153a98c75269071f6520603f5c96ef55
Gevorkyan Albert <abogevorgian@gmail.com>
2025-12-01 18:10:38 +0400

    register di

---
+++ WorkSchedulingSystemSln/WorkSchedulingSystem.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using WorkSchedulingSystem.Application.Common.Settings;
+using WorkSchedulingSystem.Application.ServiceContracts;
+using WorkSchedulingSystem.Application.Services;
+
+namespace WorkSchedulingSystem.Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IUserService, UserService>();
+
+        services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+         .AddJwtBearer(options =>
+         {
+             var jwt = configuration.GetSection("JwtSettings").Get<JwtSettings>();
+
+             options.TokenValidationParameters = new TokenValidationParameters
+             {
+                 ValidateIssuer = true,
+                 ValidateAudience = true,
+                 ValidateLifetime = true,
+                 ValidateIssuerSigningKey = true,
+
+                 ValidIssuer = jwt.Issuer,
+                 ValidAudience = jwt.Audience,
+                 IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key))
+             };
+         });
+
+
+        return services;
+    }
+}
