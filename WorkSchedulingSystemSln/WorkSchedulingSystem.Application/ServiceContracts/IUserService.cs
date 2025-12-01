b3212b84890fee4f57bf07933fd7f0631a8d493d
Gevorkyan Albert <abogevorgian@gmail.com>
2025-12-01 18:09:19 +0400

    add auth and user service contracts

---
+++ WorkSchedulingSystemSln/WorkSchedulingSystem.Application/ServiceContracts/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using WorkSchedulingSystem.Application.Common;
+using WorkSchedulingSystem.Application.DTO.User;
+using WorkSchedulingSystem.Domain.Entities;
+
+namespace WorkSchedulingSystem.Application.ServiceContracts;
+
+public interface IUserService
+{
+    Task<ApiResult<User>> CreateUserAsync(UserRegisterDto userRegister);
+    Task<string> LoginAsync(LoginDto loginDto);
+}
