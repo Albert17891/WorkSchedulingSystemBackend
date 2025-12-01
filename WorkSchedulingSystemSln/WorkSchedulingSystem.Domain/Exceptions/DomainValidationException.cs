bfd57462275a5b12f5d1fa9869ac2a923448e0a9
Gevorkyan Albert <abogevorgian@gmail.com>
2025-12-01 16:18:40 +0400

    add domain exceptions

---
+++ WorkSchedulingSystemSln/WorkSchedulingSystem.Domain/Exceptions/DomainValidationException.cs
@@ -0,0 +1,16 @@
+﻿namespace WorkSchedulingSystem.Domain.Exceptions;
+
+public class DomainValidationException : Exception
+{
+    public DomainValidationException()
+    {
+    }
+    public DomainValidationException(string message)
+        : base(message)
+    {
+    }
+    public DomainValidationException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
