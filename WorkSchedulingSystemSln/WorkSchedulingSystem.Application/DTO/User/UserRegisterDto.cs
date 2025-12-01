937df9ad26be31a8113915517b5a42b7347ffacf
Gevorkyan Albert <abogevorgian@gmail.com>
2025-12-01 18:09:58 +0400

    add Dto

---
+++ WorkSchedulingSystemSln/WorkSchedulingSystem.Application/DTO/User/UserRegisterDto.cs
@@ -0,0 +1,12 @@
+﻿using WorkSchedulingSystem.Domain.Enums;
+
+namespace WorkSchedulingSystem.Application.DTO.User;
+
+public record UserRegisterDto(
+    string FirstName,
+    string LastName,
+    Gender Gender,
+    string Email,
+    string Password,
+    string ConfirmPassword,
+    DateTime BirthDate);
