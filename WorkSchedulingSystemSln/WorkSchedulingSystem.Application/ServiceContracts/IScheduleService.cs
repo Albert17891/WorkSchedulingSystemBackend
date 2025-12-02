4a7c81eedc77f888d7da2535468c1c21b50020e9
Gevorkyan Albert <abogevorgian@gmail.com>
2025-12-02 18:02:37 +0400

    add new schedule service

---
+++ WorkSchedulingSystemSln/WorkSchedulingSystem.Application/ServiceContracts/IScheduleService.cs
@@ -0,0 +1,16 @@
+﻿using WorkSchedulingSystem.Application.Common;
+using WorkSchedulingSystem.Application.DTO.Schedule;
+using WorkSchedulingSystem.Domain.Enums;
+
+namespace WorkSchedulingSystem.Application.ServiceContracts;
+
+public interface IScheduleService
+{
+    Task<ApiResult<object>> CreateScheduleAsync(CreateScheduleDto createScheduleDto, Guid userId);
+    Task<ApiResult<object>> UpdateScheduleAsync(UpdateScheduleDto updateScheduleDto, Guid userId);
+    Task<ApiResult<object>> DeleteScheduleAsync(Guid scheduleId);
+    Task<ApiResult<IEnumerable<ScheduleDetailsDto>>> GetPendingSchedulesAsync();
+    Task<ApiResult<object>> ChangeScheduleStatus(Guid scheduleId, ScheduleStatus status);
+    Task<ApiResult<IEnumerable<ScheduleDetailsDto>>> GetSchedulesByUserIdAsync(Guid userId);
+    Task<ApiResult<IEnumerable<ScheduleDetailsDto>>> GetAllScheduleAsync();
+}
