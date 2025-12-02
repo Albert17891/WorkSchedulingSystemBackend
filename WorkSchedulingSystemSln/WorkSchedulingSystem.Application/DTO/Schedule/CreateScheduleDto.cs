99d44834cd8c751c3756b4fb3ad7f419bcaae52c
Gevorkyan Albert <abogevorgian@gmail.com>
2025-12-02 18:00:06 +0400

    add schedule dtos

---
+++ WorkSchedulingSystemSln/WorkSchedulingSystem.Application/DTO/Schedule/CreateScheduleDto.cs
@@ -0,0 +1,9 @@
+﻿using WorkSchedulingSystem.Application.DTO.Job;
+
+namespace WorkSchedulingSystem.Application.DTO.Schedule;
+
+public record CreateScheduleDto(  
+    DateTime Date,
+    List<CreateJobDto> Jobs
+);
+
---
+++ WorkSchedulingSystemSln/WorkSchedulingSystem.Application/DTO/Schedule/ScheduleDetailsDto.cs
@@ -0,0 +1,11 @@
+﻿using WorkSchedulingSystem.Domain.Enums;
+
+namespace WorkSchedulingSystem.Application.DTO.Schedule;
+
+public record ScheduleDetailsDto
+(
+    Guid ScheduleId,
+    DateTime Date,
+    ScheduleStatus Status,
+    List<Job.JobDetailsDto> Jobs
+);
