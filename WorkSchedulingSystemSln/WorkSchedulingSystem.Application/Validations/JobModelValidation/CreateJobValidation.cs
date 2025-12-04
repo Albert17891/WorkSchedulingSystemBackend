888332916ffb9312dba28b3803f21298403c9614
Gevorkyan Albert <abogevorgian@gmail.com>
2025-12-04 09:36:35 +0300

    add validations

---
+++ WorkSchedulingSystemSln/WorkSchedulingSystem.Application/Validations/JobModelValidation/CreateJobValidation.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using WorkSchedulingSystem.Application.DTO.Job;
+
+namespace WorkSchedulingSystem.Application.Validations.JobModelValidation;
+
+public class CreateJobValidation : AbstractValidator<CreateJobDto>
+{
+    public CreateJobValidation()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty()
+            .MaximumLength(100)
+            .WithMessage("Job title is required and must be <= 100 characters.");
+
+        RuleFor(x => x.Description)
+            .NotEmpty()
+            .WithMessage("Description is required.");
+
+        RuleFor(x => x.Duration)
+            .NotNull()
+            .WithMessage("Duration is required");
+    }
+}
